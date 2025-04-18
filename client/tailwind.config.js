4fc77f14fb60711b257fb8606a3434a9682d4b14
jha1501 <pranjaljha48@gmail.com>
2025-04-19 05:08:48 +0530

    Intitalized the project directory

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // If you're using an index.html
+    "./src/**/*.{js,ts,jsx,tsx}", // All JavaScript/TypeScript/JSX/TSX files in the src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
